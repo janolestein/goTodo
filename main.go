0037c478c5d1e7efd5a3e8dfc34e51112e0e729f
Jan-Ole Stein <janolestein@gmail.com>
2024-08-19 21:14:09 +0200

    add: moveToNext

--- main.go
+++ main.go
@@ -12,18 +12,18 @@ var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 var modelStyle = lipgloss.NewStyle().
 	Margin(1, 2).
-    Padding(1,1).
-    Border(lipgloss.HiddenBorder())
+	Padding(1, 1).
+	Border(lipgloss.HiddenBorder())
 var focusedModelStyle = lipgloss.NewStyle().
 	Margin(1, 2).
-    Padding(1,1).
+	Padding(1, 1).
 	Border(lipgloss.RoundedBorder())
 
-type focusedModel int 
+type focusedModel int
 
-const(
-    listModel focusedModel = iota
-    inputModel
+const (
+	listModel focusedModel = iota
+	inputModel
 )
 
 var kanbanModel *model
@@ -43,20 +43,26 @@ type item struct {
 	prio  int
 }
 
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
+func (i item) Title() string { return i.title }
+func (i item) Description() string {
+	if len(i.desc) > 25 {
+		return i.desc[:25] + "..."
+	} else {
+		return i.desc
+	}
+}
 func (i item) FilterValue() string { return i.title }
 
 type model struct {
 	list    []list.Model
 	focused status
-    width int
-    height int
+	width   int
+	height  int
 }
 
 func main() {
-    lists := initalModel()
-    kanbanModel = &lists
+	lists := initalModel()
+	kanbanModel = &lists
 	p := tea.NewProgram(lists, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf(" Alas, there has been an Error: %v", err)
@@ -70,8 +76,8 @@ func initalModel() model {
 		item{title: "learn Rust", desc: "Try to learn some Rust", id: 2, prio: 2},
 		item{title: "do some Leetcode", desc: "do some Leetcode Problems", id: 3, prio: 3},
 	}
-    defList := list.New(items, list.NewDefaultDelegate(),0,0)
-    defList.SetShowHelp(false)
+	defList := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	defList.SetShowHelp(false)
 	m := model{list: []list.Model{defList, defList, defList}}
 	m.list[todo].Title = "ToDo"
 	m.list[inProgress].Title = "In Progress"
@@ -101,9 +107,26 @@ func (m *model) goToPrev() {
 	}
 }
 
+func (m *model) moveToNext() tea.Msg {
+
+	selItem := m.list[m.focused].SelectedItem()
+	if selItem != nil {
+        selItem = selItem.(item)
+		index := m.list[m.focused].Index()
+		m.list[m.focused].RemoveItem(index)
+		if m.focused == todo {
+			m.list[inProgress].InsertItem(len(m.list[inProgress].Items())-1, selItem)
+		} else if m.focused == inProgress {
+			m.list[done].InsertItem(len(m.list[done].Items())-1, selItem)
+		} else if m.focused == done {
+			m.list[todo].InsertItem(len(m.list[todo].Items())-1, selItem)
+		}
+	}
+    m.goToNext()
+	return nil
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	// var cmds []tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -112,17 +135,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 
 		case "right", "l":
-            m.goToNext()
-        case "left", "h":
-            m.goToPrev()
-        case "n":
-            return NewForm(), nil 
+			m.goToNext()
+		case "left", "h":
+			m.goToPrev()
+		case "n":
+			return NewForm(), nil
+		case "m":
+			return m, m.moveToNext
 		}
 
 	case tea.WindowSizeMsg:
 		height, width := focusedModelStyle.GetFrameSize()
-        m.width = width
-        m.height = height
+		m.width = width
+		m.height = height
 		for i := range m.list {
 			m.list[i].SetSize(msg.Width, msg.Height-height)
 		}
@@ -130,11 +155,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 	var cmd tea.Cmd
 
-    m.list[m.focused], cmd = m.list[m.focused].Update(msg)
-	// for i := range m.list {
-	// 	m.list[i], cmd = m.list[i].Update(msg)
-	// 	cmds = append(cmds, cmd)
-	// }
+	m.list[m.focused], cmd = m.list[m.focused].Update(msg)
 	return m, cmd
 }
 
@@ -147,7 +168,7 @@ func (m model) View() string {
 			views = append(views, modelStyle.Render(m.list[i].View()))
 		}
 	}
-    s := lipgloss.JoinHorizontal(lipgloss.Center, views...) + "\n\n"
+	s := lipgloss.JoinHorizontal(lipgloss.Center, views...) + "\n\n"
 
 	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, s)
 }
